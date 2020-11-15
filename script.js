1ef2824873165110099e5036c5190a5a7ff3cf49
Yaroslav <ypakhachuk@gmail.com>
2020-11-15 20:58:07 +0200

    for you

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "Eman Sabir" && b=="27-06-1996")
-  	{
-  	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
-  	
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "Marinessa" && b == "15111994") {
+  window.location.href = "start.html";
+} else {
+  alert("invalid details");
+  window.location.href = "index.html";
+}
